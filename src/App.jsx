95ecfcdc4e83a5d982d6bc9735ac7ed9ca3f4df7
Vu Duc Tiep <vuductiep@live.com>
2023-10-09 15:47:16 +0900

    Create initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/sign-in'} element={<SignIn />} />
+        <Route path={'/sign-up'} element={<SignUp />} />
+        <Route path={'/about'} element={<About />} />
+        <Route path={'/profile'} element={<Profile />} />
+      </Routes>
       <h1 className={'text-red-500'}>App</h1>
-    </>
-  )
+    </BrowserRouter>)
 }
 
 export default App
