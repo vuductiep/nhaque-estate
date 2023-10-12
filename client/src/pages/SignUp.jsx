4be95c3e4821a476a72fe7bc5928d9d59f7d3504
Vu Duc Tiep <vuductiep@gaion.kr>
2023-10-12 22:15:02 +0900

    add signin functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -53,7 +53,7 @@ export default function SignUp() {
                id={'username'} onChange={handleChange}/>
         <input type="text" placeholder={'email'} className={'border p-3 rounded-lg'}
                id={'email'} onChange={handleChange}/>
-        <input type="text" placeholder={'password'} className={'border p-3 rounded-lg'}
+        <input type="password" placeholder={'password'} className={'border p-3 rounded-lg'}
                id={'password'} onChange={handleChange}
         />
         <button disabled={loading} className={'bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'}>
