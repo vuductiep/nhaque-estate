e5ffecd4035f61f53ec15015a0e725807773147d
vuductiep <vuductiep@live.com>
2023-11-18 00:28:05 +0900

    Complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -121,6 +121,22 @@ export default function Profile() {
     }
   }
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE'
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        console.log(data.message)
+        return
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   useEffect(() => {
     if (file) {
@@ -204,8 +220,14 @@ export default function Profile() {
                 </p>
               </Link>
               <div className={'flex flex-col items-center'}>
-                <button className={'text-red-700 uppercase'}>Delete</button>
-                <button className={'text-green-700 uppercase'}>Edit</button>
+                <button onClick={() => handleListingDelete(listing._id)}
+                        className={'text-red-700 uppercase'}>
+                  Delete
+                </button>
+                <button onClick={() => handleListingEdit(listing._id)}
+                        className={'text-green-700 uppercase'}>
+                  Edit
+                </button>
               </div>
             </div>
           ))}
