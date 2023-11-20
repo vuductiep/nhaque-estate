7a03bbccd5ac3711813757685726abe15b975ac7
vuductiep <vuductiep@live.com>
2023-11-20 23:02:38 +0900

    Create the listing item component and show listings

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -100,7 +100,7 @@ function Listing() {
               {
                 listing.offer && (
                   <p className={'bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'}>
-                    ${+listing.regularPrice - +listing.discountPrice}
+                    ${+listing.regularPrice - +listing.discountPrice} OFF
                   </p>
                 )
               }
