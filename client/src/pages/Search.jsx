5ab8c48bc1bee6cf32d1bfb4d9cb357790611d88
vuductiep <vuductiep@live.com>
2023-11-20 23:18:17 +0900

    Add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,7 +16,7 @@ function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  console.log(listings)
+  const [showMore, setShowMore] = useState(false)
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search)
@@ -52,6 +52,9 @@ function Search() {
         const searchQuery = urlParams.toString()
         const res = await fetch(`/api/listing/get?${searchQuery}`)
         const data = await res.json()
+        if (data.length > 8) {
+          setShowMore(true)
+        }
         setListings(data)
         setLoading(false)
       }
@@ -94,6 +97,20 @@ function Search() {
     urlParams.set('order', sidebardata.order);
     const searchQuery = urlParams.toString()
     navigate(`/search?${searchQuery}`)
+  }
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length
+    const startIndex = numberOfListings
+    const urlParams = new URLSearchParams(location.search)
+    urlParams.set('startIndex', startIndex)
+    const searchQuery = urlParams.toString()
+    const res = await fetch(`/api/listing/get?${searchQuery}`)
+    const data = await res.json()
+    if (data.length < 9) {
+      setShowMore(false)
+    }
+    setListings([...listings, ...data])
   }
 
   return (
@@ -191,6 +208,14 @@ function Search() {
           {!loading && listings && listings.map((listing) => (
             <ListingItem key={listing._id} listing={listing}/>
           ))}
+
+          {showMore && (
+            <button onClick={onShowMoreClick}
+                    className={'text-green-700 hover:underline p-7 text-center w-full'}
+            >
+              Show more
+            </button>
+          )}
         </div>
       </div>
     </div>
